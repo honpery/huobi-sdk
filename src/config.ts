56bd7fb211ed2b09c3b48ee00e59b8d1e8ec408c
honpery <honpery@gmail.com>
2017-12-18 16:43:12 +0800

    add xhttp error handler logic.

--- src/config.ts
+++ src/config.ts
@@ -52,6 +52,106 @@ export const APIs = initApis({
                 desc: '查询系统当前时间',
             },
         },
+
+        // 用户资产API
+        account: {
+            accounts: {
+                path: '/v1/account/accounts',
+                desc: '查询当前用户的所有账户(即account_id)',
+            },
+
+            balance: {
+                path: '/v1/account/accounts/:account_id/balance',
+                desc: '查询指定账户的余额',
+            },
+        },
+
+        // 交易API
+        order: {
+
+            place: {
+                path: '/v1/order/orders/place',
+                desc: '下单',
+            },
+
+            cancel: {
+                path: '/v1/order/orders/:order_id/submitcancel',
+                desc: '申请撤销一个订单请求',
+            },
+
+            cancels: {
+                path: '/v1/order/orders/batchcancel',
+                desc: '批量撤销订单',
+            },
+
+            list: {
+                path: '/v1/order/orders',
+                desc: '查询当前委托、历史委托',
+            },
+
+            detail: {
+                path: '/v1/order/orders/:order_id',
+                desc: '查询某个订单详情',
+            },
+
+            matchs: {
+                path: '/v1/order/matchresults',
+                desc: '查询当前成交、历史成交',
+            },
+
+            match: {
+                path: '/v1/order/orders/:order_id/matchresults',
+                desc: '查询某个订单的成交明细',
+            },
+
+        },
+
+        // 借贷交易API
+        margin: {
+            in: {
+                path: '/v1/dw/transfer-in/margin',
+                desc: '现货账户划入至借贷账户',
+            },
+
+            out: {
+                path: '/v1/dw/transfer-out/margin',
+                desc: '借贷账户划出至现货账户',
+            },
+
+            apply: {
+                path: '/v1/margin/orders',
+                desc: '申请借贷',
+            },
+
+            repay: {
+                path: '/v1/margin/orders/:order_id/repay',
+                desc: '归还借贷',
+            },
+
+            orders: {
+                path: '/v1/margin/loan-orders',
+                desc: '借贷订单',
+            },
+
+            account: {
+                path: '/v1/margin/accounts/balance',
+                desc: '借贷账户详情',
+            },
+        },
+
+        // 虚拟币提现API
+        dw: {
+            create: {
+                path: '/v1/dw/withdraw/api/create',
+                desc: '申请提现虚拟币',
+            },
+
+            cancel: {
+                path: '/v1/dw/withdraw-virtual/:wd_id/cancel',
+                desc: '申请取消提现虚拟币',
+            },
+        },
+
     },
 
     ws: {
--- src/fetch/rest/_base.ts
+++ src/fetch/rest/_base.ts
@@ -1,6 +1,15 @@
 import { Http } from 'xhttp-js';
 import { APIs, SERVERs } from '../../config';
 
+export interface ResData<T> {
+    status: 'ok' | 'error';
+    ch?: string;
+    ts?: number;
+    data?: T;
+    'err-code'?: string;
+    'err-msg'?: string;
+}
+
 export class BaseAPI {
 
     protected http = new Http({
@@ -11,4 +20,36 @@ export class BaseAPI {
 
     protected apis = APIs;
 
+    protected async json<T>(res: Response) {
+        const result: {
+            code: number | string;
+            message: string;
+            ch?: string;
+            ts?: number;
+            data?: T;
+        } = { code: 0, message: 'success' };
+
+        if (res.status >= 400 && res.status <= 599) {
+            result.code = res.status;
+            result.message = res.statusText;
+            return result;
+        }
+
+        const json = await res.json() as ResData<T>;
+
+        if (json.status === 'ok') {
+            result.data = json.data;
+            result.ch = json.ch;
+            result.ts = json.ts;
+            return result;
+        }
+
+        if (json.status === 'error') {
+            result.code = json['err-code'] || '';
+            result.message = json['err-msg'] || '';
+            return result;
+        }
+
+        return result;
+    }
 }
